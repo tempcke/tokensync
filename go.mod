0112bdebee1fbdd03048fecf137bc58e29ac2273
redeamsbouaazzi <sami.bouaazzi@redeam.com>
2022-03-28 21:08:25 -0500

    implemented pgrepo, pgrepo tests, and a mock pgrepo for the token keeper

--- go.mod
+++ go.mod
@@ -4,13 +4,24 @@ go 1.17
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/jackc/pgx/v4 v4.11.0
 	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.7.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.8.1 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.6 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.7.0 // indirect
+	github.com/jackc/puddle v1.1.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
+	golang.org/x/crypto v0.0.0-20210322153248-0c34fe9e7dc2 // indirect
+	golang.org/x/sys v0.0.0-20220328115105-d36c6a25d886 // indirect
+	golang.org/x/text v0.3.3 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- pgrepo.go
+++ pgrepo.go
@@ -2,22 +2,91 @@ package tk
 
 import (
 	"context"
+	"errors"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"math/rand"
 )
 
-type PgRepo struct {}
+const (
+	DefaultTable = "token"
+	Rows         = `val, expires, created`
+	FieldCreated = `created`
+)
 
-func (r PgRepo) GetToken(ctx context.Context) (Token, error) {
-	panic("implement me")
+type PgRepo struct {
+	pool       *pgxpool.Pool
+	table      string
+	lockNumber int
 }
 
-func (r PgRepo) StoreToken(ctx context.Context, token Token) error {
-	panic("implement me")
+func NewPgRepo(pool *pgxpool.Pool) (*PgRepo, error) {
+	if pool == nil {
+		return nil, errors.New("pool is nil")
+	}
+
+	return &PgRepo{
+		pool:       pool,
+		table:      DefaultTable,
+		lockNumber: rand.Intn(100),
+	}, nil
 }
 
-func (r PgRepo) Lock() {
-	panic("implement me")
+func (r *PgRepo) GetToken(ctx context.Context) (Token, error) {
+	var (
+		token  Token
+		_query = `SELECT ` + Rows +
+			` FROM ` + r.table +
+			` ORDER BY ` + FieldCreated +
+			` DESC LIMIT 1;`
+	)
+	if err := r.pool.QueryRow(ctx, _query).Scan(
+		&token, //FIXME
+	); err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
 
-func (r PgRepo) Unlock() {
-	panic("implement me")
+func (r *PgRepo) StoreToken(ctx context.Context, token Token) error {
+	var _query = `INSERT INTO   ` + r.table +
+		` (` + Rows + `) ` +
+		`VALUES ($1, $2, $3);`
+	if _, err := r.pool.Exec(
+		ctx,
+		_query,
+		token.String(),
+		token.Created(),
+		token.Expires(),
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PgRepo) Lock(ctx context.Context) error {
+	if _, err := r.pool.Exec(ctx, `SELECT pg_advisory_lock($1);`, r.lockNumber); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PgRepo) Unlock(ctx context.Context) error {
+	if _, err := r.pool.Exec(ctx, `SELECT pg_advisory_unlock($1);`, r.lockNumber); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *PgRepo) withTable(table string) *PgRepo {
+	r.table = table
+	return r
+}
+
+func (r *PgRepo) withLockNumber(lockNumber int) *PgRepo {
+	r.lockNumber = lockNumber
+	return r
 }
